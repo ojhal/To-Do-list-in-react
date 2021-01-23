65e7452811c74aee12a9af47a5d13ec729c75fcb
Ojhal Patwa <patkarojhal26@gmail.com>
2021-01-23 14:15:44 +0530

    add state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: []
+    }
+
+  }
+
   render() {
     return (
       <div className="App">
-        Hello world!
+
       </div>
 
     )
